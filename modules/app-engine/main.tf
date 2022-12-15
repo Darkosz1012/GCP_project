1861e54a5168d57ee614fd7d80b9457d74fafca6
Darkosz <darek10121998db@gmail.com>
2022-12-15 21:59:12 +0100

    add terraform module for app engine

---
+++ modules/app-engine/main.tf
@@ -0,0 +1,52 @@
+resource "google_service_account" "custom_service_account" {
+  account_id   = "my-account"
+  display_name = "Custom Service Account"
+}
+
+resource "google_project_iam_member" "gae_api" {
+  project = google_service_account.custom_service_account.project
+  role    = "roles/compute.networkUser"
+  member  = "serviceAccount:${google_service_account.custom_service_account.email}"
+}
+
+resource "google_project_iam_member" "storage_viewer" {
+  project = google_service_account.custom_service_account.project
+  role    = "roles/storage.objectViewer"
+  member  = "serviceAccount:${google_service_account.custom_service_account.email}"
+}
+
+resource "google_app_engine_standard_app_version" "app_v1" {
+  version_id = "v1"
+  service    = "default"
+  runtime    = "nodejs18"
+
+  entrypoint {
+    shell = "node ./index.js"
+  }
+
+  deployment {
+    zip {
+      source_url = "https://storage.googleapis.com/${var.bucket_name}/${var.object_name}"
+    }
+  }
+
+  env_variables = var.env_variables
+
+  automatic_scaling {
+    max_concurrent_requests = 10
+    min_idle_instances      = 1
+    max_idle_instances      = 1
+    min_pending_latency     = "1s"
+    max_pending_latency     = "5s"
+
+    standard_scheduler_settings {
+      target_cpu_utilization        = 0.8
+      target_throughput_utilization = 0.8
+      min_instances                 = var.min_instances
+      max_instances                 = var.max_instances
+    }
+  }
+
+  delete_service_on_destroy = true
+  service_account           = google_service_account.custom_service_account.email
+}
